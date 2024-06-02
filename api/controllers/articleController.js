12081a09c94ceb204290bb66813c60239dda581e
RUHAMYA Elie <elieruhamya20@gmail.com>
2024-06-02 23:25:34 +0200

    new update

--- api/controllers/articleController.js
+++ api/controllers/articleController.js
@@ -1,17 +1,57 @@
+
+const Article = require("../models/Article")
+
 module.exports = {
-    get: function (req, res, next) {
-        res.send("articles");
+    get: async (req, res, next) => {
+        try {
+            const all = await Article.find({})
+            res.send(all);
+        } catch (err) {
+            console.log(err);
+        }
     },
-    getId: function (req, res, next) {
-        res.send("get id");
+    getId: async (req, res, next) => {
+        const id = req.params.id
+        
+        try {
+            const article = await Article.findById( { _id: id} );
+            if (article) res.send( article );
+            if (!article) res.send({ getData: false });
+        } catch (error) {
+            res.send({ getData: false });
+        }
     },
-    create: function (req, res, next) {
-        res.send("create articles");
+    create: async (req, res, next) => {
+        const articleSaved = new Article(req.body)
+        try {
+            const tmpSaved = await articleSaved.save()
+            res.status(201).send(tmpSaved)
+        } catch (e) {
+            res.status(400).send({ dataCreated: false })
+        }
     },
-    update: function (req, res, next) {
-        res.send("update articles");
+    update: async (req, res, next) => {
+        const id = req.params.id;
+        try {
+            const update_article = await Article.findByIdAndUpdate({ _id: id }, req.body)
+            res.status(201).send({ dataUpdated: true })
+        } catch (err) {
+            res.status(400).send({ dataUpdated: false })
+        }
     },
-    delete: function (req, res, next) {
-        res.send("delete articles");
+    delete: async (req, res, next) => {
+        const id = req.params.id;
+
+        try {
+            const articles = await Article.deleteOne({ _id: id })
+            if (articles) res.status(201).send({ dataDeleted: true });
+            if (!articles) res.status(400).send({ dataDeleted: false });
+        } catch (err) {
+            res.status(400).send({ dataDeleted: false });
+        }        
     },
+    search: async (req, res, next) => { res.status(201).send("search") },
+    pagination: async (req, res, next) => { res.status(201).send("pagination") },
+    sort: async (req, res, next) => { res.status(201).send("sort") },
 }
+
---
+++ api/models/Article.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose")
+
+// Schema allow us to call functions
+// or middlewares *before and *after requests...
+const articleSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        minlength: 3,
+        maxlength: 50
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: {
+            values: ['expository', 'persuasive', 'narrative', 'descriptive'],
+            message: "{VALUE} is not supported"
+        }
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true,
+        min: 4,
+        max: 50
+    },
+    // user_id: {}
+})
+
+const Article = mongoose.model('Article', articleSchema);
+
+module.exports = Article;
