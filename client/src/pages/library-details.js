0cbbd44d773f1f134b197d85d034b18ee7f37006
RUHAMYA Elie <elieruhamya20@gmail.com>
2024-06-19 02:19:12 +0200

    new update

--- client/src/pages/library-details.js
+++ client/src/pages/library-details.js
@@ -1,7 +1,18 @@
 import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
 
 export const LibraryDetails = () => {
-  return (
-    <div>LibraryDetails</div>
+  return (<section className="py-5">
+            <div className="container">
+              <div>
+                <ul>
+                  <li><Link to={'ressources'}>Ressources</Link></li>
+                  <li><Link to={'followers'} href='#'>Followers</Link></li>
+                  <li><Link to={'managment'} href='#'>Managment</Link></li>
+                </ul>
+              </div>
+              <Outlet/>
+            </div>
+          </section>
   )
 }
---
+++ client/src/pages/ressources.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Ressources = () => {
+  return (
+    <div>Ressources
+        <div>Manage your ressources</div>
+    </div>
+  )
+}
