53dd1bcb8440bd51a885750f18eb608ad328d976
RUHAMYA Elie <elieruhamya20@gmail.com>
2024-07-15 17:02:27 +0200

    implement unfollow* button

--- client/src/components/elements/followers-items.jsx
+++ client/src/components/elements/followers-items.jsx
@@ -1,7 +1,20 @@
+import axios from 'axios'
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 
-export const FollowersItems = ({followers}) => {
+export const FollowersItems = ({followers, updateItems, role}) => {
+  const {id} = useParams()
+  const unFollow = (followerId) => {
+    axios.post(`http://localhost:9000/followers/unfollow`, {
+      params: { 
+        _id: followerId,
+        libraryId: id 
+      }
+    }).then((res) => {
+      updateItems(res.data)
+    })
+  }
+
   return (
     <div>
         {followers.map((follower, i) => <div key={i} className='d-flex justify-content-between border rounded' style={{padding: '10px', marginBottom: `${i == followers.length -1 ? '-10px' : '10px'}`}}>
@@ -16,7 +29,7 @@ export const FollowersItems = ({followers}) => {
               <span style={{fontSize: '12px'}}>{follower.user_email}</span>
             </div>
           </div>
-          <button className='btn btn-body border btn-sm'>Unfollow</button>
+          <button disabled={role ? false : true} onClick={() => unFollow(follower._id)} className='btn btn-body border btn-sm'>Unfollow</button>
         </div>)} 
       </div>
   )
--- server/controllers/followerController.js
+++ server/controllers/followerController.js
@@ -4,31 +4,41 @@ const Library = require("../models/Library")
 const {get} = require("../controllers/libraryControler")
 
 module.exports = {
-    add: async (req, res, next) => {
-        const agg = [
-            {
-              '$lookup': {
-                'from': 'followers', 
-                'localField': '_id', 
-                'foreignField': 'libraryId', 
-                'as': 'followers'
-              }
+  add: async(req, res, next) => {
+      const agg = [
+          {
+            '$lookup': {
+              'from': 'followers', 
+              'localField': '_id', 
+              'foreignField': 'libraryId', 
+              'as': 'followers'
             }
-          ];
+          }
+        ];
 
-        const follower = new Follower(req.body);
+      const follower = new Follower(req.body);
 
-        try {
-            const save_follower = await follower.save();
-            await get(req, res, next);
-            // res.status(200).send(await Library.aggregate(agg));
-        } catch (error) {}
-    },
-    library_followers: async(req, res, next) => {
-        const libraryId = req.params.id;
-        const followers = await Follower.find({
-            libraryId: libraryId
-        });
-        res.status(200).send(followers);
-    }
+      try {
+          const save_follower = await follower.save();
+          await get(req, res, next);
+          // res.status(200).send(await Library.aggregate(agg));
+      } catch (error) {}
+  },
+  library_followers: async(req, res, next) => {
+      const libraryId = req.params.id;
+      const followers = await Follower.find({
+          libraryId: libraryId
+      });
+      res.status(200).send(followers);
+  },
+  unfollow: async(req, res, next) => {
+    const params = req.body.params
+    try {
+      const followers = await Follower.findByIdAndDelete(params)
+      res.status(200).send(await Follower.find({
+        libraryId: req.body.params.libraryId
+      }))
+    } catch (error) { }
+    console.log("deleted...")
+  }
 }
